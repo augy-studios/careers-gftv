be2d88785d760f4cc8d07bd60def2092aee020c1
Augy <ateoks@gmail.com>
2026-08-26 02:43:25 +0800

    phase 8 part 11m

--- main-site/sw.js
+++ main-site/sw.js
@@ -27,7 +27,7 @@
 // the files that exist as well. A precache entry naming a deleted file makes
 // cache.addAll reject and the whole install fail.
 
-const VERSION = 'careers-gftv-phase8-v74';
+const VERSION = 'careers-gftv-phase9-v75';
 
 self.addEventListener('install', () => {
   // Nothing to precache yet.
